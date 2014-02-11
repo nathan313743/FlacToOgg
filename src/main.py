ea5af4ddc8c3b8e7a6b62477eb5b880133bef2ba
nathan313743 <nathandevelopmail@gmail.com>
2014-02-11 17:16:11 +0000

    :P

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import os
+
+foundFileList = []
+
+for root, dirs, files in os.walk("/home/nathan/Eclipse"):
+    for file in files:
+        if(file.endswith(".flac")):
+            foundFileList.append(os.path.join(root, file))
+            #print(os.path.join(root, file))
+
+for file in foundFileList:
+    print(file)
+
